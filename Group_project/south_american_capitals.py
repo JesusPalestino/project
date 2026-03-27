2147020f51680ab4f753baf8b3a66bf85004f377
JesusPalestino <ym8503tj@go.minnstate.edu>
2026-03-27 11:05:08 -0500

    Add files via upload

---
+++ Group_project/south_american_capitals.py
@@ -0,0 +1,15 @@
+south_capitals = {
+    'Argentina': 'Buenos Aires',
+    'Boliva': 'Sucre',
+    'Brazil': 'Brasillia',
+    'Chile': 'Santiago',
+    'Colombia': 'Bogota',
+    'Ecuador': 'Quito',
+    'Guyana': 'Georgetown',
+    'Paraguay': 'Asuncion',
+    'Peru': 'LIma',
+    'Suriname': 'Paramaribo',
+    'Uruguay': 'Montevideo',
+    'Venezuela': 'Caracas'
+
+}
