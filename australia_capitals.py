b2077c9d16df6c920f56ff1134bd5a3c8e2dd390
JesusPalestino <ym8503tj@go.minnstate.edu>
2026-04-23 09:54:59 -0500

    Add files via upload

---
+++ australia_capitals.py
@@ -0,0 +1,10 @@
+australia_capitals = {
+    'New South Wales': 'Sydney',
+    'Victoria': 'Melbourne',
+    'Queensland': 'Brisbane',
+    'South Australia': 'Adelaide',
+    'Western Australia': 'Perth',
+    'Tasmania': 'Hobart',
+    'Australian Capital Territory': 'Canberra',
+    'Northern Territory': 'Darwin'
+}
